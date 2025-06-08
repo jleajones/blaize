7303f1031e43fdc9c1d0d4dcb57644f2384e815c
Jason Lea-Jones <jason@careymarcel.com>
2025-06-08 14:54:42 -0400

    🔌 expose blaize-client

--- packages/blaize-client/src/index.ts
+++ packages/blaize-client/src/index.ts
@@ -1,1 +1,8 @@
+import { createClient } from './client';
 export const blaizeClientVersion = '0.1.0';
+
+const bc = {
+  create: createClient,
+  version: blaizeClientVersion,
+};
+export default bc;
