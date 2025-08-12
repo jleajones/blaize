6a02facdabd3c2ddfea739b0c883b8fcea89a50f
Jason Lea-Jones <jason@careymarcel.com>
2025-08-11 22:16:16 -0400

    ✨ updated the playground with working example

--- apps/playground/src/client.ts
+++ apps/playground/src/client.ts
@@ -1,16 +1,18 @@
-// import bc from '@blaizejs/client';
+import bc from '@blaizejs/client';
 
-// import { type AppType } from './app-type.js';
+import { routes } from './app-type.js';
 
-// const _client = bc.create<AppType>('https://api.example.com'); // fully typed client
+const client = bc.create('https://api.example.com', routes); // fully typed client
 
-// export async function testClient() {
-//   try {
-//     const _helloResponse = await _client.$get.getHello();
-//     const _helloPostResponse = await _client.$post.postHello();
-//     console.log('GET Hello Response:');
-//     console.log('GET Hello Response:', _helloResponse.message); // fully typed response
-//   } catch (error) {
-//     console.log(error); // fully typed error BlaizeError
-//   }
-// }
+console.log('Client created:', client);
+
+export async function testClient() {
+  try {
+    const _helloResponse = await client.$get.getHello();
+    const _helloPostResponse = await client.$post.postHello({ body: { name: 'World' } });
+    console.log('GET Hello Response:');
+    console.log('GET Hello Response:', _helloResponse.name); // fully typed response
+  } catch (error) {
+    console.log(error); // fully typed error BlaizeError
+  }
+}
--- apps/playground/src/routes/hello.ts
+++ apps/playground/src/routes/hello.ts
@@ -4,12 +4,12 @@ import { z } from 'zod';
 export const getHello = createGetRoute({
   schema: {
     response: z.object({
-      message: z.string(),
+      name: z.string(),
     }),
   },
   handler: async () => {
     return {
-      message: 'Hi, it is BlaizeJS!',
+      name: 'Hi, it is BlaizeJS!',
     };
   },
 });
