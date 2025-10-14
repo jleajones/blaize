8b09ed6f8935a3eddabc3ebbc9209a84bd46cb6d
Jason Lea-Jones <jason@careymarcel.com>
2025-10-14 17:08:43 -0400

    🏗️ scaffold the routes

---
+++ plugins/metrics/src/routes.ts
@@ -0,0 +1,14 @@
+/**
+ * Metrics routes for exposing metrics endpoints
+ *
+ * This file will contain route handlers for:
+ * - GET /metrics - Prometheus format metrics
+ * - GET /metrics/json - JSON format metrics
+ *
+ * Currently empty, to be populated in future tasks.
+ *
+ * @module @blaizejs/plugin-metrics/routes
+ */
+
+// Placeholder - routes will be added in future tasks
+export {};
