8441398c414edb9f007b7d0cad50adc8a346384b
Jason Lea-Jones <jason@careymarcel.com>
2025-12-31 02:12:07 -0500

    🔨 fix playground

--- apps/playground/src/routes/metrics/index.ts
+++ apps/playground/src/routes/metrics/index.ts
@@ -3,4 +3,6 @@ import { metricsJsonRoute } from '@blaizejs/plugin-metrics';
 import { appRouter } from '../../app-router';
 
 // JSON snapshot at GET /metrics/json
-export const GET = appRouter.get(metricsJsonRoute);
+export const GET = appRouter.get({
+  handler: metricsJsonRoute,
+});
