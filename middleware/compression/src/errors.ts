38dfd97e49517d3587eb158a75014ff8644acc35
Jason Lea-Jones <jason@careymarcel.com>
2026-03-06 18:33:37 -0500

    🔧 [T2] Fix getCorrelationId import path

--- middleware/compression/src/errors.ts
+++ middleware/compression/src/errors.ts
@@ -1,5 +1,5 @@
 import { BlaizeError, ErrorType } from '@blaize-types/errors';
-import { getCorrelationId } from '../../../packages/blaize-core/src/tracing/correlation';
+import { getCorrelationId } from 'blaizejs';
 
 interface CompressionConfigurationErrorDetails {
   field: string;
